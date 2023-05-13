e6723074632ad3bb04a9f335bc5caf3c9429a279
august <kingaugust20@icloud.com>
2023-05-12 21:35:36 -0700

    little changes

---
+++ src/robots.js
@@ -0,0 +1,66 @@
+export const robots = [
+    {
+        id:1,
+        name:'Leann Graham',
+        username:'Bret',
+        email:'sincere.april@blitz'
+    },
+    {
+        id:2,
+        name:'Scott Peterson',
+        username:'Molly',
+        email:'Kolybaba.March@Tv'
+    },
+    {
+        id:3,
+        name:'Preye Samson',
+        username:'August',
+        email:'PreyeAugust@gmail.com'
+    },
+    {
+        id:4,
+        name:'Osilama Ochuwa',
+        username:'Ninos Candy',
+        email:'candy@connect.tv'
+    },
+    {
+        id:5,
+        name:'Suen Pada',
+        username:'BOD',
+        email:'BOD@korgy.org'
+    },
+    {
+        id:6,
+        name:'Lucky Bella',
+        username:'Bellz',
+        email:'Bellz.net@yesnai.net'
+    },
+    {
+        id:7,
+        name:'Damian Paul',
+        username:'Dammy',
+        email:'Dammz@outlook.com'
+    },
+    {
+        id:8,
+        name:'Percy Graham',
+        username:'Percy',
+        email:'underdog@blitz'
+    },
+    {
+        id:9,
+        name:'Preston Nigolo',
+        username:'Dpress',
+        email:'Dpress.bo@blitz'
+    },
+    {
+        id:10,
+        name:'Sharp Dwayne',
+        username:'SharpD',
+        email:'sharp.april@blitz'
+    }
+]
+
+
+
+export default robots;
